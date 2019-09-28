8c5bd5b24b945c7c926f5c8efabaee8ff54537e2
Qylningque <qylningque@outlook.com>
2019-09-28 18:01:43 +0800

    完结订单创建和完结的权限校验

--- api-gateway/src/main/java/com/linco/apigateway/filter/AuthFilter.java
+++ api-gateway/src/main/java/com/linco/apigateway/filter/AuthFilter.java
@@ -1,12 +1,17 @@
 package com.linco.apigateway.filter;
 
+import com.linco.apigateway.constant.RedisConstant;
+import com.linco.apigateway.utils.CookieUtil;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_DECORATION_FILTER_ORDER;
@@ -21,6 +26,10 @@ import static org.springframework.cloud.netflix.zuul.filters.support.FilterConst
  */
 @Component
 public class AuthFilter extends ZuulFilter {
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
     /**
      * 路由参数校验，在PRE前置路由
      * @return
@@ -54,11 +63,31 @@ public class AuthFilter extends ZuulFilter {
         RequestContext requestContext = RequestContext.getCurrentContext();
         HttpServletRequest request = requestContext.getRequest();
         /**
-         * /order/create 创建订单，只能买家访问
-         * /order/finish 完结订单，只能卖家访问
+         * /order/create 创建订单，只能买家访问（cookie里有openid）
+         * /order/finish 完结订单，只能卖家访问（cookie理由token，并且对应redis中的值）
          * /product/list 订单列表，卖家买家都可以访问
          */
-
+        //1.创建订单权限过滤
+        if ("/order/order/create".equals(request.getRequestURI())){
+            Cookie cookie = CookieUtil.get(request,"openid");
+            if (cookie == null || StringUtils.isEmpty(cookie.getValue())){
+                //返回权限不足
+                requestContext.setSendZuulResponse(false);
+                requestContext.setResponseStatusCode(HttpStatus.UNAUTHORIZED.value());
+            }
+        }
+        //2.完结订单权限过滤
+        if ("/order/order/finish".equals(request.getRequestURI())){
+            Cookie cookie = CookieUtil.get(request,"token");
+            //判断cookie是否为空，并判断redis中是否存在
+            if (cookie == null
+                    || StringUtils.isEmpty(cookie.getValue())
+                    || StringUtils.isEmpty(stringRedisTemplate.opsForValue().get(String.format(RedisConstant.TOKEN_TEMPLATE,cookie)))){
+                //返回权限不足
+                requestContext.setSendZuulResponse(false);
+                requestContext.setResponseStatusCode(HttpStatus.UNAUTHORIZED.value());
+            }
+        }
         return null;
     }
 }
---
+++ api-gateway/src/main/java/com/linco/apigateway/utils/CookieUtil.java
@@ -0,0 +1,45 @@
+package com.linco.apigateway.utils;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @Classname: CookieUtil
+ * @description: CookieUtil
+ * @Author: Create by qingyulin(寧缺) qylningque@outlook.com
+ * @Date: 2019-09-27 19:31
+ * @Version 1.0
+ */
+public class CookieUtil {
+    /**
+     * 设置cookie
+     * @param response
+     * @param name
+     * @param value
+     * @param maxAge
+     */
+    public static void set(HttpServletResponse response,
+                           String name,
+                           String value,
+                           int maxAge){
+        Cookie cookie = new Cookie(name, value);
+        cookie.setPath("/");
+        cookie.setMaxAge(maxAge);
+        response.addCookie(cookie);
+    }
+
+    public static Cookie get(HttpServletRequest request,
+                             String name){
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null){
+            for (Cookie cookie : cookies) {
+                if (name.equals(cookie.getName())){
+                    return cookie;
+                }
+            }
+        }
+        return null;
+    }
+
+}
